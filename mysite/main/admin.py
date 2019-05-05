7696c7f76668fe38aede4f751bba916ebbbce5dd
Matias Montes <mati.montes@hotmail.com>
2019-05-05 03:09:04 -0300

    Clases implementadas
    Intenté crear las clases. No pude lograr validar correctamente las fechas.

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -1,14 +1,35 @@
 from django.contrib import admin
-from .models import Residencia, Subasta, HotSale
+from .models import Residencia, Subasta, HotSale, Imagen
 # Register your models here.
 
 #class ResidenciaAdmin(admin.ModelAdmin):
 #	fields = ["nombre",
 #				"tutorial_published",
 #				"tutorial_content"]
+class ImagenInline(admin.TabularInline):
+	model = Imagen
+	extra = 0
+	verbose_name_plural = 'imágenes'
+
+class SubastaAdmin(admin.ModelAdmin):
+	fields = ["residencia","precio_reserva","precio_inicial","dia_inicial","inicio_de_subasta"]
+
+class SubastaInLine(admin.TabularInline):
+	model = Subasta
+	extra = 0
+	fields = ["precio_reserva","precio_inicial","dia_inicial","inicio_de_subasta"]
+
+class HotSaleInLine(admin.TabularInline):
+	model = HotSale
+	extra = 0
+	fields = ["precio_reserva","dia_inicial"]
+
+class ResidenciaAdmin(admin.ModelAdmin):
+	inlines = [
+		ImagenInline, SubastaInLine, HotSaleInLine,
+	]
 
 
-
-admin.site.register(Residencia)
-admin.site.register(Subasta)
+admin.site.register(Residencia,ResidenciaAdmin)
+admin.site.register(Subasta,SubastaAdmin)
 admin.site.register(HotSale)
--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from datetime import date
+from datetime import date, timedelta
 
 # Create your models here.
 class Residencia(models.Model):
@@ -16,17 +16,38 @@ class Residencia(models.Model):
 	def __str__(self):
 		return self.nombre
 
+	def semanaDisponible(self,fecha):
+		disponible = True
+		for semana in (self.subastas + self.hotsales):
+			if semana.coincide(fecha):
+				disponible = False
+				break
+		return disponible
+
 class Semana(models.Model):
-	dia_inicial = models.DateField()
-	dia_final = models.DateField()
+	dia_inicial = models.DateField() 
 	precio_reserva = models.FloatField()
+
+	def __str__(self):
+		return self.residencia.nombre + " - De: " + self.dia_inicial.isoformat() + " a: " + self.dia_final().isoformat()
+
+	def dia_final(self):
+		return self.dia_inicial + timedelta(days=7)
+
+	def coincide(self,fecha):
+		return fecha <= self.dia_inicial - timedelta(days=7) and fecha >= self.dia_final()
 
 	class Meta:
 		abstract = True
 
 class Subasta(Semana):
 	precio_inicial = models.FloatField()
+	inicio_de_subasta = models.DateField()
 	residencia = models.ForeignKey('Residencia',on_delete=models.CASCADE,related_name='subastas')
 
 class HotSale(Semana):
 	residencia = models.ForeignKey('Residencia',on_delete=models.CASCADE,related_name='hotsales')
+
+class Imagen(models.Model):
+	residencia = models.ForeignKey(Residencia,on_delete=models.CASCADE,related_name='imagenes')
+	imagen = models.ImageField(upload_to=('fotos/'))
