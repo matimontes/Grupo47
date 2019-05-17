da19411a71f6eeede3ba14038a0dc91bb50323b7
lautaropastorino <lautaro.pastorino@hotmail.com>
2019-05-17 02:33:30 -0300

    Acomode la logica de utilizacion de css y js
    Cambie los colores ademas porque agregue el logo. Igual despues podemos ver que color nos gusta

---
+++ mysite/static/scripts/buscar_residencias.js
@@ -0,0 +1,16 @@
+const calendario = document.querySelector('.datepicker');
+hoy = new Date();
+M.Datepicker.init(calendario, {
+  minDate: hoy, //fecha minima para elegir
+  defaultDate: hoy,
+  format: "dd mmmm, yyyy",
+  showDaysInNextAndPreviousMonths: true,
+  showMonthAfterYear: true,
+  //Traduccion
+  i18n: {
+    months: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
+    monthsShort: ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Set", "Oct", "Nov", "Dic"],
+    weekdays: ["Domingo","Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"],
+    weekdaysShort: ["Dom","Lun", "Mar", "Mie", "Jue", "Vie", "Sab"],
+    weekdaysAbbrev: ["D","L", "M", "M", "J", "V", "S"]
+}});
