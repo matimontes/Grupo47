fa4bb86874286b14c4d5b8e9b6c4389e2573910d
lautaropastorino <lautaro.pastorino@hotmail.com>
2019-05-18 02:48:53 -0300

    Estoy agregando filtros pero no me salio todavia. Subo el progreso.

--- mysite/static/scripts/buscar_residencias.js
+++ mysite/static/scripts/buscar_residencias.js
@@ -1,3 +1,7 @@
+alert("a");
+const Elems = document.querySelector('select');
+M.FormSelect.init(Elems, {});
+
 const calendario = document.querySelector('.datepicker');
 hoy = new Date();
 M.Datepicker.init(calendario, {
@@ -12,5 +16,4 @@ M.Datepicker.init(calendario, {
     monthsShort: ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Set", "Oct", "Nov", "Dic"],
     weekdays: ["Domingo","Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"],
     weekdaysShort: ["Dom","Lun", "Mar", "Mie", "Jue", "Vie", "Sab"],
-    weekdaysAbbrev: ["D","L", "M", "M", "J", "V", "S"]
-}});
+    weekdaysAbbrev: ["D","L", "M", "M", "J", "V", "S"]}});
