a8a6f2eac68f5d01a31799dae453abd7feafff2d
Matias Montes <mati.montes@hotmail.com>
2019-06-10 12:49:18 -0300

    Arreglé el problema de la creación del superuser

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from datetime import date, timedelta
+from django.utils import timezone
 
 from django.core.mail import send_mail
 from django.contrib.auth.models import PermissionsMixin
@@ -48,7 +49,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     last_name = models.CharField(_('apellido'), max_length=30, blank=True)
     date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
     is_active = models.BooleanField(_('active'), default=True)
-    date_of_birth = models.DateTimeField(_('fecha de nacimiento'))
+    date_of_birth = models.DateTimeField(_('fecha de nacimiento'), default=timezone.now)
     nacionalidad = models.CharField(max_length=50, blank=True)
     creditos = models.IntegerField(default=0)
     premium = models.BooleanField(_('premium estatus'), default=False)
