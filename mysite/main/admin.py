5eef3108239f9286c4e0c2205e7957c9e0ec5bb2
lautaropastorino <lautaro.pastorino@hotmail.com>
2019-06-17 14:42:38 -0300

    Cambie el encabezado de la pestaña
    Antes decia sitio de Django o algo asi

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -328,6 +328,7 @@ admin.site.unregister(Group)
 
 admin.site.index_template = 'admin/mysite/index.html'
 admin.site.site_header = 'Administración de HSH'
+admin.site.site_title = 'Administración de HSH'
 admin.site.register(SemanaReservada,SemanaReservadaAdmin)
 admin.site.register(Residencia,ResidenciaAdmin)
 admin.site.register(Subasta,SubastaAdmin)
